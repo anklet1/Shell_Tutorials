827ef1040de11829265c3c24840aa127a3c5ffbd
anklet1 <flekufelix@gmail.com>
2023-04-30 08:41:37 +0000

    Integer check

--- integer_check.sh
+++ integer_check.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-#THIS SCRIPT CHECKS FOR POSITIVE OR NEGATIVE INTEGERS
-
-#Prompt user to enter a number
+#The script first prompts the user to enter a number. Then, it uses an if statement to check if the number is greater than 0. If it is, the script outputs that the number is positive. If the number is not greater than 0, the script moves on to the next statement, which is an if-elif statement. Here, the script checks if the number is less than 0. If it is, the script outputs that the number is negative. Finally, if the number is neither greater than 0 nor less than 0, the script uses an else statement to output that the number is zero.
 
 echo "Please enter a number"
 
